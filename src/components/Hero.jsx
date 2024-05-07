18a85aec79f302339e371fc13cb243d96cf4d142
Adonai Romero <adonai@Adonais-MacBook-Pro.local>
2024-05-07 17:13:45 -0600

    up to desktop image

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import Computers from "./canvas/Computers";
+
+const Hero = () => {
+	return (
+		<section className="relative w-full h-screen mx-auto">
+			<div
+				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+			>
+				<div className="flex flex-col justify-center items-center mt-5">
+					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div>
+
+				<div>
+					<h1 className={`${styles.heroHeadText} text-white`}>
+						Greetings, I'm{" "}
+						<span className="text-[#915eff]">Adonai</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						I adeptly merge front-end UI/UX design principles with
+						robust back-end development expertise to craft dynamic
+						and user-centric digital solutions.
+					</p>
+				</div>
+			</div>
+
+			<ComputersCanvas />
+		</section>
+	);
+};
+
+export default Hero;
