632ceaf3e62b553ad05ecb0b62cad54419146dba
Adonai Romero <adonai@Adonais-MacBook-Pro.local>
2024-05-08 22:18:21 -0600

    almost done. need to update projects, testimonials, and add stars to contact

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+	index,
+	name,
+	description,
+	tags,
+	image,
+	source_code_link,
+}) => {
+	return (
+		<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+			<Tilt
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+			>
+				<div className="relative q-full h-[230px]">
+					<img
+						src={image}
+						alt={name}
+						className="w-full h-full object-cover rounded-2xl"
+					/>
+
+					<div className=" absolute inset-0 flex justify-end m-3 card-img_hover">
+						<div
+							onClick={() =>
+								window.open(source_code_link, "_blank")
+							}
+							className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+						>
+							<img
+								src={github}
+								alt="github"
+								className="w-1/2 h-1/2 object-contain"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="mt-5">
+					<h3 className="text-white font-bold text-[24px]">{name}</h3>
+					<p className="mt-2 text-secondary text-[14px]">
+						{description}
+					</p>
+				</div>
+				<div className="mt-4 flex flex-wrap gap-2">
+					{tags.map((tag) => (
+						<p
+							key={tag.name}
+							className={`text-[14px] ${tag.color}`}
+						>
+							#{tag.name}
+						</p>
+					))}
+				</div>
+			</Tilt>
+		</motion.div>
+	);
+};
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>My Code</p>
+				<h2 className={styles.sectionHeadText}>Projects.</h2>
+			</motion.div>
 
-export default Works
+			<div className="w-full flex">
+				<motion.p
+					variants={fadeIn("", "", 0.1, 1)}
+					className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+				>
+					I blend front-end elegance with back-end robustness. Using
+					HTML, CSS, and JavaScript, I create intuitive interfaces,
+					while Node.js or Django, along with databases like MongoDB
+					or PostgreSQL, power the server-side logic. I also integrate
+					APIs for seamless communication and prioritize clean code
+					practices, version control with Git, and deployment via
+					platforms like Heroku or Render. My projects not only
+					showcase technical proficiency but also reflect a holistic
+					approach to problem-solving and software craftsmanship.
+				</motion.p>
+			</div>
+
+			<div className="mt-20 flex flex-wrap gap-7">
+				{projects.map((project, index) => (
+					<ProjectCard
+						key={`project-${index}`}
+						index={index}
+						{...project}
+					/>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default Works;
--- src/constants/index.js
+++ src/constants/index.js
@@ -54,7 +54,7 @@ const services = [
 		icon: backend,
 	},
 	{
-		title: "Content Creator",
+		title: "API Developer",
 		icon: creator,
 	},
 ];
@@ -116,150 +116,147 @@ const technologies = [
 
 const experiences = [
 	{
-		title: "React.js Developer",
-		company_name: "Starbucks",
+		title: "Software Engineer Trainee",
+		company_name: "Coding Temple",
 		icon: starbucks,
 		iconBg: "#383E56",
-		date: "March 2020 - April 2021",
+		date: "February 2024 - April 2024",
 		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
+			"I underwent an intensive curriculum focusing on front-end and back-end development, collaborating on real-world projects and embracing agile practices. ",
+			"I delved into various programming languages, frameworks, and methodologies, honing my ability to translate conceptual ideas into tangible, functional solutions.",
+			"I actively engaged in code reviews, debugging sessions, and agile development practices, refining my proficiency in problem-solving and fostering a keen eye for detail. This equipped me with the technical expertise necessary and instilled in me a passion for creativity, collaboration, and excellence in software craftsmanship.",
 		],
 	},
 	{
-		title: "React Native Developer",
-		company_name: "Tesla",
+		title: "Facilitator | CEO",
+		company_name: "Adonai & Kaylee | Holistic Wellness",
 		icon: tesla,
 		iconBg: "#E6DEDD",
-		date: "Jan 2021 - Feb 2022",
+		date: "July 2022 - Present",
 		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
+			"I adeptly juggled multiple roles to ensure the seamless operation of the establishment, laying a foundation that resonates with the responsibilities of a Software Developer. Focused on client satisfaction, I oversaw operations, led teams, and formed strategic partnerships.",
+			"I enhanced our digital presence through website development, optimizing user experience and operations.",
+			"This multifaceted role honed my skills in project management, problem-solving, and adaptability, developing  a strong candidate for a Software Developer role.",
 		],
 	},
 	{
-		title: "Web Developer",
-		company_name: "Shopify",
+		title: "Barber",
+		company_name: "Finesse Barber Studio",
 		icon: shopify,
 		iconBg: "#383E56",
-		date: "Jan 2022 - Jan 2023",
+		date: "Feb 2021 - August 2022",
 		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
+			"As a skilled Barber, I honed precision, creativity, and client satisfaction, traits essential for a Software Developer.",
+			"My meticulous approach and effective communication ensured personalized experiences, akin to understanding user needs in software development.",
+			"Adaptability to industry trends reflects readiness for technology's fast pace. With skills in problem-solving, creativity, and customer-centricity, I'm well-equipped for success as a Software Developer.",
 		],
 	},
-	{
-		title: "Full stack Developer",
-		company_name: "Meta",
-		icon: meta,
-		iconBg: "#E6DEDD",
-		date: "Jan 2023 - Present",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
-	},
+	// {
+	// 	title: "Full stack Developer",
+	// 	company_name: "Meta",
+	// 	icon: meta,
+	// 	iconBg: "#E6DEDD",
+	// 	date: "Jan 2023 - Present",
+	// 	points: [
+	// 		"Developing and maintaining web applications using React.js and other related technologies.",
+	// 		"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+	// 		"Implementing responsive design and ensuring cross-browser compatibility.",
+	// 		"Participating in code reviews and providing constructive feedback to other developers.",
+	// 	],
+	// },
 ];
 
 const testimonials = [
-	{
-		testimonial:
-			"I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-		name: "Sara Lee",
-		designation: "CFO",
-		company: "Acme Co",
-		image: "https://randomuser.me/api/portraits/women/4.jpg",
-	},
-	{
-		testimonial:
-			"I've never met a web developer who truly cares about their clients' success like Rick does.",
-		name: "Chris Brown",
-		designation: "COO",
-		company: "DEF Corp",
-		image: "https://randomuser.me/api/portraits/men/5.jpg",
-	},
-	{
-		testimonial:
-			"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-		name: "Lisa Wang",
-		designation: "CTO",
-		company: "456 Enterprises",
-		image: "https://randomuser.me/api/portraits/women/6.jpg",
-	},
+	// {
+	// 	testimonial:
+	// 		"I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+	// 	name: "Sara Lee",
+	// 	designation: "CFO",
+	// 	company: "Acme Co",
+	// 	image: "https://randomuser.me/api/portraits/women/4.jpg",
+	// },
+	// {
+	// 	testimonial:
+	// 		"I've never met a web developer who truly cares about their clients' success like Rick does.",
+	// 	name: "Chris Brown",
+	// 	designation: "COO",
+	// 	company: "DEF Corp",
+	// 	image: "https://randomuser.me/api/portraits/men/5.jpg",
+	// },
+	// {
+	// 	testimonial:
+	// 		"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+	// 	name: "Lisa Wang",
+	// 	designation: "CTO",
+	// 	company: "456 Enterprises",
+	// 	image: "https://randomuser.me/api/portraits/women/6.jpg",
+	// },
 ];
 
 const projects = [
-	{
-		name: "Car Rent",
-		description:
-			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "mongodb",
-				color: "green-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "pink-text-gradient",
-			},
-		],
-		image: carrent,
-		source_code_link: "https://github.com/",
-	},
-	{
-		name: "Job IT",
-		description:
-			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "restapi",
-				color: "green-text-gradient",
-			},
-			{
-				name: "scss",
-				color: "pink-text-gradient",
-			},
-		],
-		image: jobit,
-		source_code_link: "https://github.com/",
-	},
-	{
-		name: "Trip Guide",
-		description:
-			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-		tags: [
-			{
-				name: "nextjs",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "supabase",
-				color: "green-text-gradient",
-			},
-			{
-				name: "css",
-				color: "pink-text-gradient",
-			},
-		],
-		image: tripguide,
-		source_code_link: "https://github.com/",
-	},
+	// 	{
+	// 		name: "Car Rent",
+	// 		description:
+	// 			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+	// 		tags: [
+	// 			{
+	// 				name: "react",
+	// 				color: "blue-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "mongodb",
+	// 				color: "green-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "tailwind",
+	// 				color: "pink-text-gradient",
+	// 			},
+	// 		],
+	// 		image: carrent,
+	// 		source_code_link: "https://github.com/",
+	// 	},
+	// 	{
+	// 		name: "Job IT",
+	// 		description:
+	// 			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+	// 		tags: [
+	// 			{
+	// 				name: "react",
+	// 				color: "blue-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "restapi",
+	// 				color: "green-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "scss",
+	// 				color: "pink-text-gradient",
+	// 			},
+	// 		],
+	// 		image: jobit,
+	// 		source_code_link: "https://github.com/",
+	// 	},
+	// 	{
+	// 		name: "Trip Guide",
+	// 		description:
+	// 			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+	// 		tags: [
+	// 			{
+	// 				name: "nextjs",
+	// 				color: "blue-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "supabase",
+	// 				color: "green-text-gradient",
+	// 			},
+	// 			{
+	// 				name: "css",
+	// 				color: "pink-text-gradient",
+	// 			},
+	// 		],
+	// 		image: tripguide,
+	// 		source_code_link: "https://github.com/",
+	// 	},
 ];
 
 export { services, technologies, experiences, testimonials, projects };
